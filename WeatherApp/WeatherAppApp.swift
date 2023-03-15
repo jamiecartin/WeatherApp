45aecfb1f5f874414a21f01ad8aecac770704058
jamiecartin <jamie.cartin@icloud.com>
2023-03-15 14:14:38 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jamie Cartin on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
